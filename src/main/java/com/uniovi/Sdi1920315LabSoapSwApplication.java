9699dbaf73de71cbd5b14287766d5bc297464878
jesQM <jezuquema@gmail.com>
2020-04-23 16:11:45 +0200

    SDI-SW-11.1 Servicio SOAP

---
+++ src/main/java/com/uniovi/Sdi1920315LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1920315LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi1920315LabSoapSwApplication.class, args);
+	}
+
+}
